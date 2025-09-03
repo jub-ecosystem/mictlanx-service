d61eb7294b2b7bd27964f2c6319ba3603019c20d
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2025-09-03 12:44:38 -0600

    Improve build script

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 readonly IMAGE_TAG=${1:-0.0.161a20}
 readonly IMAGE=nachocode/mictlanx:router-${IMAGE_TAG}
+readonly PUSH_FLAG=${2:-1}
+
+
+
 docker build -f ./Dockerfile -t $IMAGE .
-docker push $IMAGE
+
+if [ "$PUSH_FLAG" -eq 1 ]; then
+	echo "Pushing image: $IMAGE"
+	docker push $IMAGE
+else
+	echo "Skipping push"
+fi 
